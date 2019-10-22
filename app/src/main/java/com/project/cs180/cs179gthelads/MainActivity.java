12f6bc5b674d8b47cdd6d892da6fe29d8854be3a
mvazq013 <35284864+mvazq013@users.noreply.github.com>
2019-10-22 12:08:39 -0700

    Added a button for photos, a handler, a listener, and StartCmeraActivity() which creates a user Intent object

--- app/src/main/java/com/project/cs180/cs179gthelads/MainActivity.java
+++ app/src/main/java/com/project/cs180/cs179gthelads/MainActivity.java
@@ -1,43 +1,80 @@
 package com.project.cs180.cs179gthelads;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URI;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+
 public class MainActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
     Button login;
+    protected Button _button;
+    protected ImageView _image;
+    protected TextView _field;
+    protected String _path;
+    protected boolean _taken;
+    protected static final String PHOTO_TAKEN = "photo_taken";
 
+    //@SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = findViewById(R.id.login);
 
         progressDialog = new ProgressDialog(this);
 
-        login.setOnClickListener(new View.OnClickListener(){
-           public void onClick(View v){
-               ServerLogin serverlogin = new ServerLogin();
-               serverlogin.execute();
-           }
-        });
+
+        _image = findViewById( R.id.image );
+        _field = findViewById( R.id.field );
+        _button = findViewById( R.id.button );
+        _button.setOnClickListener( new ButtonClickHandler() );
+
+        _path = Environment.getExternalStorageDirectory() + "/images/make_machine_example.jpg";
+        // May need to change the path later on
+
     }
+
+    public class ButtonClickHandler implements View.OnClickListener
+    {
+        public void onClick( View view ){
+            startCameraActivity();
+        }
+    }
+
+    protected void startCameraActivity()
+    {
+        File file = new File( _path );
+        Uri outputFileUri = Uri.fromFile( file );
+
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE );
+        intent.putExtra( MediaStore.EXTRA_OUTPUT, outputFileUri );
+
+        startActivityForResult( intent, 0 );
+    }
+
 
      private class ServerLogin extends AsyncTask<String,String,String>{
 
@@ -74,4 +111,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getBaseContext(),""+message, Toast.LENGTH_LONG).show();
         }
     }
+
 }
