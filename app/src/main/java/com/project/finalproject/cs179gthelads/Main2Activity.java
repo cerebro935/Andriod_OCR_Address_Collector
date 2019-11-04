7cbe7baaff067f19b09f92bbc77f517595eefaba
cerebro935 <angel.guzman935@gmail.com>
2019-11-03 19:32:26 -0800

    created submit button.

---
+++ app/src/main/java/com/project/finalproject/cs179gthelads/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.project.finalproject.cs179gthelads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    protected Button _submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
