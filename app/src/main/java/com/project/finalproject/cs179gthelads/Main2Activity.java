a210209151a292779cad0063985d7a2bf0b88050
cerebro935 <angel.guzman935@gmail.com>
2019-11-03 20:05:09 -0800

    Added method to test connection to server.

--- app/src/main/java/com/project/finalproject/cs179gthelads/Main2Activity.java
+++ app/src/main/java/com/project/finalproject/cs179gthelads/Main2Activity.java
@@ -1,15 +1,39 @@
 package com.project.finalproject.cs179gthelads;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
     protected Button _submitButton;
+    protected Button _connectButton;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        progressDialog = new ProgressDialog(this);
+        _submitButton = findViewById( R.id.button2 );
+        _connectButton = findViewById(R.id.button4);
+
+        _connectButton.setOnClickListener(new connectClick());
+//        _submitButton.setOnClickListener( new Server );
+    }
+
+    public class connectClick implements View.OnClickListener{
+        public void onClick( View view ){
+            startConnectActivity();
+        }
+    }
+
+    private void startConnectActivity(){
+        Server server = new Server();
+        String message = Server.connect();
+        Toast.makeText(getBaseContext(),""+message, Toast.LENGTH_LONG).show();
     }
 }
