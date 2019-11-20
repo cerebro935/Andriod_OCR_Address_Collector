c5ab2866c39f5e9704ea6aa6af6e4662dfb64cc7
cerebro935 <angel.guzman935@gmail.com>
2019-11-19 17:49:15 -0800

    Implemented return address parsing with Jgeocoder API for better results.

--- app/src/main/java/com/project/finalproject/cs179gthelads/Server.java
+++ app/src/main/java/com/project/finalproject/cs179gthelads/Server.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
+import net.sourceforge.jgeocoder.AddressComponent;
+import net.sourceforge.jgeocoder.us.AddressParser;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -14,6 +17,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.lang.Integer;
+import java.util.Map;
 
 public class Server extends AsyncTask<String, String, String>{
 
@@ -27,6 +31,7 @@ public class Server extends AsyncTask<String, String, String>{
         String street = "";
         String city = "";
         String state = "";
+        String direction = "";
         String url = "";
         int postal = 0;
         boolean supporterFlag = true;
@@ -41,52 +46,37 @@ public class Server extends AsyncTask<String, String, String>{
         //remove some symbols from the submitted text
         details = details.replace("\n", " ");
         details = details.replace(",", " ");
+        Log.d("Details", details);
 
-        //split text by spaces
-        String[] splitDetails = details.split("\\s+");
-        length = splitDetails.length;
+        //use Jgeocode API to parse the address
+        Map<AddressComponent, String> parsedAddr = AddressParser.parseAddress(details);
 
-        //postal code is the last object of a return address
-        postal = Integer.parseInt(splitDetails[length - 1]);
-        //state is right before postal code. Some states are two words, but normal people use abbreviations anyways.
-        state = splitDetails[length - 2];
-        //this is only for cities that are one word.
-        // Need to figure out a method for cities whose names consist of more than one word.
-        city = splitDetails[length - 3];
-
-        //this loop is for the supporters name and street address
-        for(int i = 0; i < (length - 3); ++i){
-            //until we find a string that contains digits, add to supporter
-            if((!splitDetails[i].matches(".*\\d.*")) && supporterFlag){
-                if(supporter.isEmpty()){
-                    supporter = supporter.concat(splitDetails[i]);
-                }
-                else{
-                    supporter = supporter.concat(" ");
-                    supporter = supporter.concat(splitDetails[i]);
-                }
-            }
-            else if(supporterFlag){//digits are found and are assumed to be the start of a street address.
-                street = street.concat(splitDetails[i]);
-                supporterFlag = false;
-                streetFlag = true;
-            }
-            else if((!splitDetails[i].matches(".*\\d.*")) && streetFlag){
-                //the rest of the objects are part of street address
-                street = street.concat(" ");
-                street = street.concat(splitDetails[i]);
-
-            }
-            else if(streetFlag){
-                streetFlag = false;
-            }
+        supporter = parsedAddr.get(AddressComponent.NAME);
+        if(supporter == null){
+            supporter = "N/A";
         }
+        city = parsedAddr.get(AddressComponent.CITY);
+        state = parsedAddr.get(AddressComponent.STATE);
+        postal = Integer.parseInt(parsedAddr.get(AddressComponent.ZIP));
+
+        //Check for street direction
+        direction = parsedAddr.get(AddressComponent.PREDIR);
+        if(direction != null){
+            street = direction+" ";
+        }
+
+        street = street+parsedAddr.get(AddressComponent.NUMBER)+" "+
+                parsedAddr.get(AddressComponent.STREET)+" "+
+                parsedAddr.get(AddressComponent.TYPE);
+
 
         Log.d("Supporter: ", supporter);
         Log.d("Street", street);
         Log.d("city", city);
         Log.d("State", state);
         Log.d("zip", String.valueOf(postal));
+
+
 
         //url for inserting into the supporter table
         url = "http://172.119.206.111/insertSupporter.php?var="+supporter;
@@ -134,6 +124,7 @@ public class Server extends AsyncTask<String, String, String>{
                 Log.e("ERROR PHP-URL-Exception", e.getMessage());
                 return "ERROR connecting to Apache Server";
             }
+
     }
 
     protected void onPostExecute(String message){
