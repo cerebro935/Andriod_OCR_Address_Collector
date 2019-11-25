9f7f00b1fcc3bb3d1b611b09f811a3e4ac32e88e
dfons007 <dfons007@ucr.edu>
2019-11-25 13:12:46 -0800

    Revising OCR, Adding OPENCV

--- app/src/main/java/com/project/finalproject/cs179gthelads/MainActivity.java
+++ app/src/main/java/com/project/finalproject/cs179gthelads/MainActivity.java
@@ -1,41 +1,38 @@
 package com.project.finalproject.cs179gthelads;
 
-import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.ColorMatrix;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,12 +43,14 @@ public class MainActivity extends AppCompatActivity {
     private TessOcr myTess;
     Dialog myDialog;
     protected String myString = "no result";
+    private Bitmap mybitmap;
 
 
 
     //@SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        OpenCVLoader.initDebug();
         myDialog = new Dialog(this);
         myTess = new TessOcr(this);
         super.onCreate(savedInstanceState);
@@ -74,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+        ImageView overlay =(ImageView)findViewById(R.id.croprec);
+        ((ViewGroup)overlay.getParent()).removeView(overlay);
+        preview.addView(overlay);
 
 
     }
@@ -83,19 +85,20 @@ public class MainActivity extends AppCompatActivity {
         TextView close;
         Button  subBtn;
         EditText myInput;
-
+        ImageView myImage;
         myDialog.setContentView(R.layout.popup);
         close = myDialog.findViewById(R.id.closetxt);
         confirmTxt = myDialog.findViewById((R.id.confirm));
-        myInput = myDialog.findViewById(R.id.userInput);
-        myInput.setText(myString);
+        //myInput = myDialog.findViewById(R.id.userInput);
+        myImage = myDialog.findViewById(R.id.Image);
+        myImage.setImageBitmap(mybitmap);
         subBtn = myDialog.findViewById(R.id.submit);
 
 
         subBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                myString = myInput.getText().toString(); //myString <--- edited text
+                //myString = myInput.getText().toString(); //myString <--- edited text
                 myDialog.dismiss();
             }
         });
@@ -190,11 +193,20 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Height", Integer.toString(options.outHeight));
             Log.d("Width", Integer.toString(options.outWidth));
             pictureFile.delete();
+            bitmap = Bitmap.createBitmap(bitmap, 1000,950,2280, 1260);
             bitmap = Bitmap.createScaledBitmap(bitmap, 960,720,false);
+            Mat tmp = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+            Utils.bitmapToMat(bitmap, tmp);
+            Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_RGB2GRAY);
+            // Processing more?
+            Imgproc.threshold(tmp,tmp,90,255,Imgproc.THRESH_BINARY);
+            Utils.matToBitmap(tmp, bitmap);
+            mybitmap = bitmap;
+
             bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
             myTess.TessInit();
             myString = myTess.readImage(bitmap); //myString <-- extracted info
-
+            Log.d("String", myString);
             //begin popup
             ShowPopup();
             mCamera.stopPreview();
